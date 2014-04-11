94c74c6f65183be06f66299d162f1a829dea5c01
skopan <tmarcais@laposte.net>
2014-04-11 14:12:34 +0200

    Ajout de l'entete du controleur d'accueil

--- Code/Programme/ControleurAccueil.rb
+++ Code/Programme/ControleurAccueil.rb
@@ -1,9 +1,23 @@
+# encoding: UTF-8
+
+require './ModeleAccueil'
+require './VueAccueil'
+require './Controleur'
+
+require 'gtk2'
+
+# Le contrôleur d'accueil permet d'accéder 
 class ControleurAccueil < Controleur
 
 	# Constructeur
 	def initialize(unJeu)
 
 		super(unJeu)
+		@modele = ModeleAccueil.new
+		@vue = VueAccueil.new(@modele)
+
+		@modele.ajouterObservateur(@vue)
+		
 	end
 
 	
