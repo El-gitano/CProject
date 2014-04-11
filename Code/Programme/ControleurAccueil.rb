54a0ef1592359bb8b507b673f1d10e20bf8207a1
skopan <tmarcais@laposte.net>
2014-04-11 15:09:07 +0200

    Definition de la methode "boutonCredit"

--- Code/Programme/ControleurAccueil.rb
+++ Code/Programme/ControleurAccueil.rb
@@ -6,8 +6,10 @@ require './Controleur'
 
 require 'gtk2'
 
-# Le contrôleur d'accueil permet d'accéder 
+# Le contrôleur d'accueil permet d'accéder aux différents menu du jeu
 class ControleurAccueil < Controleur
+
+	public_class_method :new
 
 	# Constructeur
 	def initialize(unJeu)
@@ -21,7 +23,8 @@ class ControleurAccueil < Controleur
 	end
 
 	@vue.boutonDeco.signal_connect("clicked"){
-	
+		@modele.sauvegarderProfil
+		changerControleur(ControleurDemarrage.new(@picross))
 	
 	}
 	
@@ -36,8 +39,36 @@ class ControleurAccueil < Controleur
 	}
 	
 	@vue.boutonCredit.signal_connect("clicked"){
-	
-	
+		
+		dialogue = Gtk::Dialog.new("Credits", @vue.window, Gtk::Dialog::DESTROY_WITH_PARENT,[Gtk::Stock::OK, Gtk::Dialog::RESPONSE_ACCEPT])
+		
+		tabNoms = Array.new
+
+		tabNoms.push(Gtk::Label.new("AYDIN Emre"))
+		tabNoms.push(Gtk::Label.new("FOUCAULT Antoine"))
+		tabNoms.push(Gtk::Label.new("GUENVER Loic"))
+		tabNoms.push(Gtk::Label.new("LANVIN Elyan"))
+		tabNoms.push(Gtk::Label.new("MARCAIS Thomas"))
+		tabNoms.push(Gtk::Label.new("RAMOLET Arthur"))
+
+		labelAnnee = Gtk::Label.new("Cree en 2014")
+		labelUniv = Gtk::Label.new("Projet Universite du Maine")
+
+		boutonOK = Gtk::Button.new("Ok", false)
+
+		# Ajout des elements a la Vbox
+		tabNoms.each{|x|
+
+			dialogue.vbox.add(x)
+		}
+
+		dialogue.vbox.add(labelAnnee)
+		dialogue.vbox.add(labeluniv)
+
+		# Affichage des elements et lancement de la fenetre
+		dialogue.show_all
+
+		dialogue.run
 	}
 	
 	@vue.boutonProfil.signal_connect("clicked"){
