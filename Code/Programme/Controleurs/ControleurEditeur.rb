ee7b956f40cc24db27d8eddb52231603d01ef4f1
skopan <tmarcais@laposte.net>
2014-04-18 14:55:02 +0200

    Ajout de methodes

--- Code/Programme/Controleurs/ControleurEditeur.rb
+++ Code/Programme/Controleurs/ControleurEditeur.rb
@@ -27,26 +27,32 @@ class ControleurEditeur < Controleur
 	}
 	
 	@vue.bouton5.signal_connect("clicked"){
-	
+		@modele.grille = GrilleEditeur.creerGrille(5)
+		@modele.lancerMaj
 	}
 	
 	@vue.bouton10.signal_connect("clicked"){
-	
+		@modele.grille = GrilleEditeur.creerGrille(10)
+		@modele.lancerMaj	
 	}
 	
 	@vue.bouton15.signal_connect("clicked"){
-	
+		@modele.grille = GrilleEditeur.creerGrille(15)
+		@modele.lancerMaj	
 	}
 	
 	@vue.bouton20.signal_connect("clicked"){
-	
+		@modele.grille = GrilleEditeur.creerGrille(20)
+		@modele.lancerMaj	
 	}
 	
 	@vue.bouton25.signal_connect("clicked"){
-	
+		@modele.grille = GrilleEditeur.creerGrille(25)
+		@modele.lancerMaj	
 	}
 	
 	# Retour a l'accueil
 	def retourAccueil
+		changerControleur(ControleurAccueil.new(@picross, @modele.profil))
 	end
 end
