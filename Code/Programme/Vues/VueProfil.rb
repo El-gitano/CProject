7f705cc058b9160699a3947dacc9100ff454a81b
hippunk <arthur.ramolet@hotmail.fr>
2014-05-11 00:23:22 +0200

    Séparation des méthodes communes ModeleEditeur/ModeleJeu dans la classe ModeleGrille

--- Code/Programme/Vues/VueProfil.rb
+++ Code/Programme/Vues/VueProfil.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require './Vues/Vue'
 
 class VueProfil < Vue
--- Code/squelette/ModeleJeu.rb
+++ Code/squelette/ModeleJeu.rb
@@ -1,4 +1,10 @@
-class ModeleJeu
+# encoding: utf-8
+
+require './Modeles/ModeleGrille'
+require './Modeles/Grilles/GrilleJeu'
+require 'date'
+
+class ModeleJeu < ModeleGrille
     @plateauJeu
     attr_reader :plateauJeu
 end
