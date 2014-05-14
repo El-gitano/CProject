3b80a2ebfa5beef882156c6af0e615d9db5b9466
Lelouch72 <Lelouch_72@hotmail.fr>
2014-05-14 17:07:54 +0200

    timer == TERMINE

--- Code/Programme/Modeles/Timer.rb
+++ Code/Programme/Modeles/Timer.rb
@@ -1,30 +1,20 @@
 #La classe timer stocke un temps qu'elle incrémente chaque seconde lorsque son thread est lancé
 class Timer
-
-		ENCOURS = 0
-		FINI = 1
-		ENPAUSE = 2
 		
-		@tempsOrigine
 		@temps
 		
-		@statutJeu
 		@label
 		@profil		
 	
 		@thread
-		
-		@heures
-		@minutes
-		@secondes
+
 
 		attr_writer :label
-		attr_reader :temps, :tempsOrigine
+		attr_reader :temps
 
 		def initialize(unTemps=0, profil)
 
-			@tempsOrigine = unTemps
-			@temps = tempsOrigine
+			@temps = unTemps
 			@profil = profil
 		end 
 
@@ -36,7 +26,6 @@ class Timer
 					sleep 1
 					@profil.getStats["temps_joue"] +=1
 					@temps +=1
-					getTime
 					setLabel
 				end
 			}
@@ -49,22 +38,24 @@ class Timer
 			@thread.kill
 		end
 
-		#Récupère les informations temporelles sous forme d'entiers
-		def getTime
-		
-			@heures = @temps/3600
-			@minutes = (@temps - (@heures*3600)) / 60
-			@secondes = @temps - (@heures*3600) - (@minutes*60)
-		end
-		
 		#Met à jour le label à partir des variables d'instances désignant le temps
 		def setLabel
-
-			h = @heures < 10 ? "0" + @heures.to_s : @heures.to_s
-			m = @minutes < 10 ? "0" + @minutes.to_s : @minutes.to_s
-			s = @secondes < 10 ? "0" + @secondes.to_s : @secondes.to_s
-			
-			@label.set_label(h + ":" + m + ":" + s)
+	
+			@label.set_label(to_s)
 			@label.show
+		end
+		
+		def to_s
+		
+			heures = @temps/3600
+			minutes = (@temps - (heures*3600)) / 60
+			secondes = @temps - (heures*3600) - (minutes*60)
+			
+			h = heures < 10 ? "0" + heures.to_s : heures.to_s
+			m = minutes < 10 ? "0" + minutes.to_s : minutes.to_s
+			s = secondes < 10 ? "0" + secondes.to_s : secondes.to_s
+			
+			return h + ":" + m + ":" + s
+			
 		end
 end
--- Code/Programme/Vues/Dialogues/DialogueInfoFinPartie.rb
+++ Code/Programme/Vues/Dialogues/DialogueInfoFinPartie.rb
@@ -35,14 +35,12 @@ class DialogueInfoFinPartie < Gtk::Dialog
 
 		# Creation des elements
 		labelInfo = Gtk::Label.new("La grille est valide ! \n")	
-		labelClic = Gtk::Label.new("Nombre de clics : ")
 		labelJoker = Gtk::Label.new("Nombre de jokers utilises : ")
 		labelTemps = Gtk::Label.new("Temps ecoule : ")
 
 		nbJokerUtilise = @modele.plateauJeu.nbJokers - @modele.plateauJeu.nbJokers
-		labelNbClic = Gtk::Label.new(@modele.profil.getStats["nombre_clics"].to_s)
 		labelNbJoker = Gtk::Label.new(nbJokerUtilise.to_s)
-		labelTempsEcoule = Gtk::Label.new(@modele.timer.tempsEcoule.to_s)
+		labelTempsEcoule = Gtk::Label.new(@modele.timer.to_s)
 
 		# Ajout des elements
 		vbox.add(hbox1)
@@ -53,11 +51,9 @@ class DialogueInfoFinPartie < Gtk::Dialog
 		
 		hbox1.add(labelInfo)
 
-		vbox1.add(labelClic)
 		vbox1.add(labelJoker)
 		vbox1.add(labelTemps)
 
-		vbox2.add(labelNbClic)	
 		vbox2.add(labelNbJoker)
 		vbox2.add(labelTempsEcoule)
 
