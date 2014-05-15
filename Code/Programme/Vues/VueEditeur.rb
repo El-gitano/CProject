a95fff211de6398848022b8489b4338cab766cd9
r00ter <antoine.fouc@gmail.com>
2014-05-15 13:31:04 +0200

    Changement de la taille de la fenetre éditeur

--- Code/Programme/Vues/VueEditeur.rb
+++ Code/Programme/Vues/VueEditeur.rb
@@ -37,7 +37,7 @@ attr_reader :boutonOuvrir, :boutonEnregistrer, :boutonImporterImage, :boutonAlea
 		super(unModele, "Éditeur")
 		@tailleGrille = @modele.grille.taille
 		
-		tailleFenetreX = 660
+		tailleFenetreX = 690
 		
 		@window.set_size_request(tailleFenetreX, 750)
 		@window.set_resizable(false) 
